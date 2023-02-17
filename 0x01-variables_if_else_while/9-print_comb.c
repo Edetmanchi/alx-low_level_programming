0825e270263187d293c2103701ae0bf54015c95d
Edetmanchi <greatstory247@gmail.com>
2023-02-17 14:36:49 -0800

    final commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,24 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+* main - main entry point
+* Return: return 0
+*/
 int main(void)
 {
-	int d;
-	int low;
+	int i;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-	putchar('\n');
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+		{
+			putchar('\n');
+			exit(0);
+		}
 
+		putchar(',');
+		putchar(' ');
+	}
 	return (0);
 }
